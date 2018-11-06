7ce503c3a0edc1032796fb3503b48e35262a83ac
Vitor Rodrigues <vitor.n.rodrigues@gmail.com>
2018-11-06 02:51:34 -0200

    Remove deadcode

--- api/routes/EndpointMockRoutes.js
+++ api/routes/EndpointMockRoutes.js
@@ -1,17 +1,9 @@
 'use strict';
-var express = require('express')
 var bodyParser = require('body-parser')
 
 module.exports = function(app) {
   var endpointMock = require('../controllers/MockController');
 
-  var mocks = express.Router()
-
-  // mocks.route
-  // mocks.use(bodyParser.json());
-  // mocks.get('/mocks', endpointMock.list)
-  // mocks.post('/mocks', endpointMock.create);
-  // todoList Routes
   app.route('/mocks')
     .all(bodyParser.json())
     .get(endpointMock.list)
